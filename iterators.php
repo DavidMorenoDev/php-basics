20168c8e8354c592bdf6a80f103360d79b94bab8
David Moreno <cesardaviddev@gmail.com>
2022-09-06 09:17:03 +0200

    iterators

--- iterators.php
+++ iterators.php
@@ -3,7 +3,28 @@
 
 <?php 
 
+for ($i = 1; $i <= 10; $i++) {
+   echo $i;
+   echo "<br>";
+}
 
+$array = array(1, 2, 3, 4);
+foreach ($array as &$valor) {
+    $valor = $valor * 2;
+    echo $valor;
+    echo "<br>";
+}
+
+$i = 1;
+while ($i <= 10) {
+    echo "<br>".$i++; 
+    
+}
+echo "<br>";
+$i = 0;
+do {
+    echo $i++;
+} while ($i < 10);
 
 ?>
 
