1a01acc54e14cb84c386c9f3ec35b29106051e92
David Moreno <cesardaviddev@gmail.com>
2022-09-06 10:06:52 +0200

    types

--- types.php
+++ types.php
@@ -1,14 +1,55 @@
-<html>
-<body>
+<?php
+// Create a file called types.php
+// This file as its name properly indicates will be used for working with the different basic types of variables and data:
+// Define a new variable and assign a value to each of the following types:
+// boolean
+// integer
+// float
+// string
+// array
+// object
+// NULL
 
-<?php 
+$boolean = True;
+var_dump($boolean);
+echo $boolean;
+
+echo"<br><br>";
+
+$integer = 5;
+var_dump($integer);
+echo $integer;
+
+
+echo"<br><br>";
+
+$float = 2.5;
+var_dump($float);
+echo $float;
+
+
+echo"<br><br>";
+
+$string = "Hello";
+var_dump($string);
 
 
 
-?>
+echo"<br><br>";
+
+$a = array("apple","banana","watermelon");
+var_dump($a);
+
+
+echo"<br><br>";
+
+$o = new DateTime();
+var_dump($o);
 
 
 
-</body>
+echo"<br><br>";
 
-</html>
+$n = NULL;
+var_dump($n);
+echo $n;
